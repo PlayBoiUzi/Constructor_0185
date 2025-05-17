9284e38717d1c105811b7c2f63279c79892a8a72
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-17 16:51:54 +0700

    Menambahkan definisi construtor

--- destruktor.cpp
+++ destruktor.cpp
@@ -10,4 +10,12 @@ public:
 ~angka(); //Destructor
 void cetakData();
  void isiData();
- };
+};
+ 
+ //Definisi member Functon
+ angka::angka(int i){ //Constructor
+ panjang = i;
+ arr = new int[i];
+ isiData();
+}
+
